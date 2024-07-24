734c102516b721fd53af380657e30f7b30590e65
Hernan <hernanvallejos94@gmail.com>
2024-07-24 16:51:20 -0400

    feature: game card, app logo and spinner are added to indicate the loading of the components

---
+++ components/Main.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { View, ActivityIndicator, FlatList, Text } from "react-native";
+import { getLatestGames } from "../lib/metacritic";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { AnimatedGameCard } from "./GameCard";
+import { Logo } from "./Logo";
+
+export function Main() {
+  const [games, setGames] = useState([]);
+  const insets = useSafeAreaInsets();
+
+  useEffect(() => {
+    getLatestGames().then((games) => {
+      setGames(games);
+    });
+  }, []);
+
+  return (
+    <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}>
+      <View style={{ marginBottom: 10 }}>
+        <Logo />
+        <Text style={{ color: "#fff", fontSize: 20, fontWeight: "bold" }}>
+          KLINY
+        </Text>
+      </View>
+      {games.length === 0 ? (
+        <ActivityIndicator size={"large"} />
+      ) : (
+        <FlatList
+          data={games}
+          keyExtractor={(game) => game.slug}
+          renderItem={({ item, index }) => (
+            <AnimatedGameCard game={item} index={index} />
+          )}
+        />
+      )}
+    </View>
+  );
+}
